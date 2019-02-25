6a28045c32c00de72d5f71a8f4b61292f961aa17
devAsad <mohd.asad_saifi@outlook.com>
2019-02-25 19:08:13 +0530

    add more changes in the UI and create Business Logic

--- app/src/main/java/com/asad/toppings/MainActivity.java
+++ app/src/main/java/com/asad/toppings/MainActivity.java
@@ -8,13 +8,103 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_Plus,btn_minus,btn_Order, btn_reset;
+    TextView tv_1;
+    CheckBox checkBox1, checkBox2;
+    int coffee = 0;
+    int total = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_Order = findViewById(R.id.btn_order);
+        btn_Plus = findViewById(R.id.btn_plus);
+        btn_reset = findViewById(R.id.btn_reset);
+
+        checkBox1 = findViewById(R.id.cb_cream);
+        checkBox2 = findViewById(R.id.cb_choco);
+        tv_1 = findViewById(R.id.tv_1);
+
+        btn_Plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                coffee++;
+                tv_1.setText(String.valueOf(coffee));
+
+            }
+        });
+
+        btn_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (coffee > 0) {
+                    coffee--;
+                    tv_1.setText(String.valueOf(coffee));
+                }
+
+
+            }
+        });
+
+        btn_Order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = coffee * 10;
+                if (checkBox1.isChecked() && coffee > 0) {
+                    total += 5 * coffee;
+                }
+                if (checkBox2.isChecked() && coffee > 0) {
+                    total += 5 * coffee;
+
+                }
+                if (coffee > 0) {
+                    if (checkBox1.isChecked() && checkBox2.isChecked()) {
+                        Toast.makeText(MainActivity.this, "You have selected " + String.valueOf(coffee)
+                                        + "Coffee with Whipped Cream and Chocolate . Your Bill is " + String.valueOf(total),
+                                Toast.LENGTH_SHORT).show();
+                    } else if (checkBox1.isChecked()) {
+                        Toast.makeText(MainActivity.this, "You have selected " + String.valueOf(coffee)
+                                        + "Coffee with Whipped Cream . Your Bill is " + String.valueOf(total),
+                                Toast.LENGTH_SHORT).show();
+                    } else if (checkBox2.isChecked()) {
+
+                        Toast.makeText(MainActivity.this, "You have selected " + String.valueOf(coffee) +
+                                        "Coffee with Chocolate . Your Bill is " + String.valueOf(total)
+                                , Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "You have selected " + String.valueOf(coffee)
+                                + "Coffee without Toppings . Your Bill is " + String.valueOf(total), Toast.LENGTH_SHORT).show();
+
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "No Coffee Selected ... ", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = 0;
+                coffee = 0;
+                tv_1.setText(String.valueOf(coffee));
+                checkBox1.setChecked(false);
+                checkBox2.setChecked(false);
+
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
